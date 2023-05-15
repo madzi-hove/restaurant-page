0dc773473e61a2b028b85a4f94d1c48cbdc61fe0
Madzi Hove <madzihove@gmail.com>
2023-05-15 17:30:12 +0200

    add style sheet import statements

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import statements
+import "./jwcomeau-reset.css";
+import "./style.css";
+
+const body = document.querySelector("body");
+const contentContainer = document.querySelector("#content");
