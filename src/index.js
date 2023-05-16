e4411b5ffa4b29e9adecaefe226dfb68d6932dd5
Madzi Hove <madzihove@gmail.com>
2023-05-16 16:08:21 +0200

    add basic styles for buttons

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// import statements
-import "./jwcomeau-reset.css";
-import "./style.css";
-
-const body = document.querySelector("body");
-const contentContainer = document.querySelector("#content");
